175276e3d58a070216644d1cc0c1f681299e0bef
bogdatan <bogdatan@24821446-e7ea-0310-81ac-b189f4507b0b>
2011-01-27 11:58:15 +0000

    Javascript function for 'select all' option in datatables
    git-svn-id: https://svn.eionet.europa.eu/repositories/Zope/trunk/eea.ldapadmin@19590 24821446-e7ea-0310-81ac-b189f4507b0b

--- eea/ldapadmin/www/account.js
+++ eea/ldapadmin/www/account.js
@@ -1,9 +1,37 @@
 $(document).ready(function(){
 	$('.hidden').hide();
+
+	/**
+	 * For tables with select all option check if all checkboxes are selected.
+	 * If one of them is unchecked then uncheck the main select all checkbox.
+	*/
+	$('.account-datatable tbody tr td.checkbox-td input[type="checkbox"]').click(function(){
+		name = $(this).attr('name').split(":list")[0];
+		if($(this).attr('class') != 'selectall'){
+			if($(this).attr('checked') == false){
+				$('.selectall').attr('checked', false);
+				$(this).attr('checked', false);
+			}else {
+				var not_all = false;
+				$.each($('.account-datatable tbody tr td.checkbox-td input[@name="' + name + '"][type="checkbox"]'), function(i, e){
+					if($(this).attr('checked') == false){
+						not_all = true;
+					}
+				});
+				if(not_all == false){
+					$('.selectall').attr('checked', true);
+				}
+			}
+		}
+	});
 });
 
 function toggleView(selector){
 	$(selector).toggle();
-	
+	return false;
+}
+
+function selectAll(name){
+	$('.account-datatable tbody tr td.checkbox-td input[@name="' + name + '"][type="checkbox"]').attr('checked', $('.selectall').attr('checked'));
 	return false;
 }
